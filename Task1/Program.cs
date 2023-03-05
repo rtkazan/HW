b7f464cc8322ca10a3be56788a07926c2ebdc16f
rtkazan <rtkazan@mail.ru>
2023-03-05 19:23:17 +0300

    hw1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.Write("Введите 1-е число: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.Write("Введите 2-е число: ");
+int b = int.Parse(Console.ReadLine()!);
+if (a < b)
+    Console.WriteLine("max = " + b);
+else if (a > b)
+    Console.WriteLine("max = " + a);    
