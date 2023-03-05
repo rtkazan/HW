60b6430ad541e4805d90d13b93c5e3e5851431e8
rtkazan <rtkazan@mail.ru>
2023-03-05 19:40:57 +0300

    hw2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.Write("Введите 1-е число: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.Write("Введите 2-е число: ");
+int b = int.Parse(Console.ReadLine()!);
+Console.Write("Введите 3-е число: ");
+int c = int.Parse(Console.ReadLine()!);
+if (a > b & a > c)
+    Console.WriteLine(a);
+else if (b > a & b > c)
+    Console.WriteLine(b);
+else if (c > a & c > b)
+    Console.WriteLine(c);
